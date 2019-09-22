585a513b6208fbe47b763f6a565e51f594aadaf7
sasithorn20 <aloneemoki@hotmail.com>
2019-09-22 15:29:28 +0700

    Commit Final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import CharacterCard from './CharacterCard.js';
-import WordCard from './WordCard.js';
+import React, { Component } from "react";
+import "./App.css";
+import WordCard from "./WordCard.js";
+import CharacterCard from "./CharacterCard.js";
 
 const word = "KOKI";
 class App extends Component {
   render() {
     return (
-      <div>
-        <WordCard value="KOKI" />
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React by 5935512014</h1>
+        </header>
+        <div className="AppHandler as Props">
+          <h1 className="App-Question"> Who am I ? </h1>
+          <WordCard value="PANGKOKI" />
+        </div>
       </div>
     );
   }
